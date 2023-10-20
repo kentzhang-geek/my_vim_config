d85519e030d0c1b6f97e9d00a21f3e4d7455b82a
KentZhang <tyzhang108@gmail.com>
2023-10-20 00:14:46 -0700

    doing a popup menu for nvim

--- init.lua
+++ init.lua
@@ -154,6 +154,17 @@ require('lazy').setup({
 'azabiong/vim-highlighter',
 'tikhomirov/vim-glsl',
 'beyondmarc/hlsl.vim',
+
+-- LSP
+'neovim/nvim-lspconfig',
+{
+    'williamboman/mason.nvim',
+    config = function ()
+        require("mason").setup()
+    end
+},
+'williamboman/mason-lspconfig.nvim',
+'nvim-lua/plenary.nvim',
 })
 
 
@@ -263,3 +274,77 @@ function CopyAbsolutePath()
 end
 vim.keymap.set('n', nleader .. 'pt', function() CopyAbsolutePath() end)
 
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+lspconfig.clangd.setup{}
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format { async = true }
+    end, opts)
+  end,
+})
+
+local popup = require("plenary.popup")
+local Win_id
+function ShowMenu(opts, cb)
+  local height = 20
+  local width = 30
+  local borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" }
+
+  Win_id = popup.create(opts, {
+        title = "MyProjects",
+        highlight = "MyProjectWindow",
+        line = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        minwidth = width,
+        minheight = height,
+        borderchars = borderchars,
+        callback = cb,
+  })
+  local bufnr = vim.api.nvim_win_get_buf(Win_id)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "q", "<cmd>lua CloseMenu()<CR>", { silent=false })
+end
+
+-- TODO
+function QuickMenu() 
+  local opts = {
+      'lookup symbol',
+  }
+  local cb = function(_, sel)
+    print("it works")
+  end
+  ShowMenu(opts, cb)
+end
+vim.keymap.set('n', nleader .. 'mm', function() QuickMenu() end)
