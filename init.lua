4fbf20ec6acf716a0511bb8883bf37e8c0cf3bb1
Kent <tyzhang108@gmail.com>
2024-01-05 12:37:17 -0800

    update auto reload

--- init.lua
+++ init.lua
@@ -1,251 +1,251 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git',
+        '--branch=stable', -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Highlighter
 vim.cmd([[
-  " default key mappings
-  let HiSet   = '<space>s'
-  let HiErase = '<space>e'
-  let HiClear = '<space>c'
-  let HiFind  = '<space>f'
+" default key mappings
+let HiSet   = '<space>s'
+let HiErase = '<space>e'
+let HiClear = '<space>c'
+let HiFind  = '<space>f'
 
-  " directory to store highlight files
-  " let HiKeywords = '~/.vim/after/vim-highlighter'
+" directory to store highlight files
+" let HiKeywords = '~/.vim/after/vim-highlighter'
 ]])
 
 -- configs
 vim.cmd([[
-  set backspace=indent,eol,start
+set backspace=indent,eol,start
+set autoread
 ]])
 
 require('lazy').setup({
-  {
-    'folke/tokyonight.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
--- For NVim completion
-'neovim/nvim-lspconfig',
-'hrsh7th/cmp-nvim-lsp',
-'hrsh7th/cmp-buffer',
-'hrsh7th/cmp-path',
-'hrsh7th/cmp-cmdline',
-'hrsh7th/nvim-cmp',
-'tpope/vim-commentary',
+    {
+        'folke/tokyonight.nvim',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd([[colorscheme tokyonight]])
+        end,
+    },
+    -- For NVim completion
+    'neovim/nvim-lspconfig',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'tpope/vim-commentary',
 
-'scrooloose/nerdtree',
-	
-'vim-airline/vim-airline',
-'vim-airline/vim-airline-themes',
--- {
+    'scrooloose/nerdtree',
+
+    'vim-airline/vim-airline',
+    'vim-airline/vim-airline-themes',
+    -- {
     -- 'phaazon/hop.nvim', -- instead of easymotion, it's faster
     -- config = function()
     --     require'hop'.setup()
     -- end,
--- },
-'easymotion/vim-easymotion',
-'rust-lang/rust.vim',
-{
-    'junegunn/fzf',
-    build = function ()
-        vim.cmd(':call fzf#install()')
-    end
-},
-'github/copilot.vim',
-'junegunn/vim-easy-align',
-'frazrepo/vim-rainbow',
-{
-    'nvim-treesitter/nvim-treesitter',  -- a better syntax highlighter
-    build = function ()
-        vim.cmd(':TSUpdate')
-    end
-},
-{
-    'gelguy/wilder.nvim',
-    config = function()
-        -- config goes here
-        local wilder = require('wilder')
-        wilder.setup({
-            modes = { ':', '/', '?', '!' },
-        })
-        wilder.set_option('renderer', wilder.popupmenu_renderer(
-        wilder.popupmenu_palette_theme({
-            -- 'single', 'double', 'rounded' or 'solid'
-            -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
-            border = 'rounded',
-            max_height = '75%',      -- max height of the palette
-            min_height = 0,          -- set to the same as 'max_height' for a fixed height window
-            prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
-            reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
-        })
-        ))
-    end,
-},
-{
-    'akinsho/bufferline.nvim',
-    config = function() 
-        require('bufferline').setup(
-        {
-            options = {
-                show_buffer_close_icons = false,
-                show_close_icon = false,
-                show_tab_indicators = true,
-                separator_style = 'thin',
-                always_show_bufferline = true,
-            }
-        }
-        )
-    end,
-},
-{
-    'nvimdev/dashboard-nvim',
-    event = 'VimEnter',
-    config = function()
-        require('dashboard').setup {
-            -- config
-            theme = 'hyper',
-            config = {
-                week_header = {
-                    enable = true,
-                },
-                packages = { enable = true }, -- show how many plugins neovim loaded
-                project = { enable = true, limit = 8, icon = 'your icon', label = ''},
-                mru = { limit = 10, icon = 'your icon', label = '', },
-                footer = {}, -- footer
-                shortcut = {
-                    { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-                    {
-                        icon = ' ',
-                        icon_hl = '@variable',
-                        desc = 'Files',
-                        group = 'Label',
-                        action = 'FZF',
-                        key = 'f',
-                    },
-                    {
-                        desc = 'Exit',
-                        group = 'Label',
-                        action = 'q',
-                        key = 'q',
-                    },
-                    -- {
-                    --   desc = ' Apps',
-                    --   group = 'DiagnosticHint',
-                    --   action = 'Telescope app',
-                    --   key = 'a',
-                    -- },
-                },
-            },
-        }
-    end,
-    dependencies = { {'nvim-tree/nvim-web-devicons'}}
-},
-{ 'stevearc/dressing.nvim' },
--- 'mattesgroeger/vim-bookmarks',
-'azabiong/vim-highlighter',
-'tikhomirov/vim-glsl',
-'beyondmarc/hlsl.vim',
-
--- LSP
-'neovim/nvim-lspconfig',
-{
-    'williamboman/mason.nvim',
-    config = function ()
-        require("mason").
-        setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "->",
-                    package_uninstalled = "x"
+    -- },
+    'easymotion/vim-easymotion',
+    'rust-lang/rust.vim',
+    {
+        'junegunn/fzf',
+        build = function ()
+            vim.cmd(':call fzf#install()')
+        end
+    },
+    'github/copilot.vim',
+    'junegunn/vim-easy-align',
+    'frazrepo/vim-rainbow',
+    {
+        'nvim-treesitter/nvim-treesitter',  -- a better syntax highlighter
+        build = function ()
+            vim.cmd(':TSUpdate')
+        end
+    },
+    {
+        'gelguy/wilder.nvim',
+        config = function()
+            -- config goes here
+            local wilder = require('wilder')
+            wilder.setup({
+                modes = { ':', '/', '?', '!' },
+            })
+            wilder.set_option('renderer', wilder.popupmenu_renderer(
+            wilder.popupmenu_palette_theme({
+                -- 'single', 'double', 'rounded' or 'solid'
+                -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+                border = 'rounded',
+                max_height = '75%',      -- max height of the palette
+                min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+                prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+                reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+            })
+            ))
+        end,
+    },
+    {
+        'akinsho/bufferline.nvim',
+        config = function() 
+            require('bufferline').setup(
+            {
+                options = {
+                    show_buffer_close_icons = false,
+                    show_close_icon = false,
+                    show_tab_indicators = true,
+                    separator_style = 'thin',
+                    always_show_bufferline = true,
                 }
             }
-        })
-    end
-},
-'williamboman/mason-lspconfig.nvim',
-'nvim-lua/plenary.nvim',
-{
-    'nvim-telescope/telescope.nvim', tag = '0.1.4',
-    -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-},
-{
-    'tomasky/bookmarks.nvim', -- after = "telescope.nvim",
-    event = "VimEnter",
-    config = function()
-        require('bookmarks').setup{
-            save_file = vim.fn.expand("$HOME/.bookmarks"), -- bookmarks save file path
-            keywords =  {
-                ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
-                ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
-                ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
-                ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
-            },
-        }
-    end
-},
-{
-    'neoclide/coc.nvim', branch = 'release',
-},
-{
-  "dhananjaylatkar/cscope_maps.nvim",
-  dependencies = {
-    "folke/which-key.nvim", -- optional [for whichkey hints]
-    "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
-    "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
-  },
-  opts = {
-      -- USE EMPTY FOR DEFAULT OPTIONS
-      -- DEFAULTS ARE LISTED BELOW
+            )
+        end,
+    },
+    {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+            require('dashboard').setup {
+                -- config
+                theme = 'hyper',
+                config = {
+                    week_header = {
+                        enable = true,
+                    },
+                    packages = { enable = true }, -- show how many plugins neovim loaded
+                    project = { enable = true, limit = 8, icon = 'your icon', label = ''},
+                    mru = { limit = 10, icon = 'your icon', label = '', },
+                    footer = {}, -- footer
+                    shortcut = {
+                        { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+                        {
+                            icon = ' ',
+                            icon_hl = '@variable',
+                            desc = 'Files',
+                            group = 'Label',
+                            action = 'FZF',
+                            key = 'f',
+                        },
+                        {
+                            desc = 'Exit',
+                            group = 'Label',
+                            action = 'q',
+                            key = 'q',
+                        },
+                        -- {
+                        --   desc = ' Apps',
+                        --   group = 'DiagnosticHint',
+                        --   action = 'Telescope app',
+                        --   key = 'a',
+                        -- },
+                    },
+                },
+            }
+        end,
+        dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    },
+    { 'stevearc/dressing.nvim' },
+    -- 'mattesgroeger/vim-bookmarks',
+    'azabiong/vim-highlighter',
+    'tikhomirov/vim-glsl',
+    'beyondmarc/hlsl.vim',
 
-      -- maps related defaults
-      disable_maps = true, -- "true" disables default keymaps
-      skip_input_prompt = false, -- "true" doesn't ask for input
-      prefix = "<leader>cs", -- prefix to trigger maps
+    -- LSP
+    'neovim/nvim-lspconfig',
+    {
+        'williamboman/mason.nvim',
+        config = function ()
+            require("mason").
+            setup({
+                ui = {
+                    icons = {
+                        package_installed = "✓",
+                        package_pending = "->",
+                        package_uninstalled = "x"
+                    }
+                }
+            })
+        end
+    },
+    'williamboman/mason-lspconfig.nvim',
+    'nvim-lua/plenary.nvim',
+    {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'tomasky/bookmarks.nvim', -- after = "telescope.nvim",
+        event = "VimEnter",
+        config = function()
+            require('bookmarks').setup{
+                save_file = vim.fn.expand("$HOME/.bookmarks"), -- bookmarks save file path
+                keywords =  {
+                    ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+                    ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+                    ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+                    ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+                },
+            }
+        end
+    },
+    {
+        'neoclide/coc.nvim', branch = 'release',
+    },
+    {
+        "dhananjaylatkar/cscope_maps.nvim",
+        dependencies = {
+            "folke/which-key.nvim", -- optional [for whichkey hints]
+            "nvim-telescope/telescope.nvim", -- optional [for picker="telescope"]
+            "nvim-tree/nvim-web-devicons", -- optional [for devicons in telescope or fzf]
+        },
+        opts = {
+            -- USE EMPTY FOR DEFAULT OPTIONS
+            -- DEFAULTS ARE LISTED BELOW
 
-      -- cscope related defaults
-      cscope = {
-          -- location of cscope db file
-          db_file = "C:/cscope_db/cscope.out",
-          -- cscope executable
-          exec = "cscope", -- "cscope" or "gtags-cscope"
-          -- choose your fav picker
-          picker = "telescope", -- "telescope", "fzf-lua" or "quickfix"
-          -- size of quickfix window
-          qf_window_size = 5, -- any positive integer
-          -- position of quickfix window
-          qf_window_pos = "bottom", -- "bottom", "right", "left" or "top"
-          -- "true" does not open picker for single result, just JUMP
-          skip_picker_for_single_result = false, -- "false" or "true"
-          -- these args are directly passed to "cscope -f <db_file> <args>"
-          db_build_cmd_args = { "-bqkv" },
-          -- statusline indicator, default is cscope executable
-          statusline_indicator = nil,
-      }
-  },
-},
-'nvim-telescope/telescope-ui-select.nvim',
+            -- maps related defaults
+            disable_maps = true, -- "true" disables default keymaps
+            skip_input_prompt = false, -- "true" doesn't ask for input
+            prefix = "<leader>cs", -- prefix to trigger maps
+
+            -- cscope related defaults
+            cscope = {
+                -- location of cscope db file
+                db_file = "C:/cscope_db/cscope.out",
+                -- cscope executable
+                exec = "cscope", -- "cscope" or "gtags-cscope"
+                -- choose your fav picker
+                picker = "telescope", -- "telescope", "fzf-lua" or "quickfix"
+                -- size of quickfix window
+                qf_window_size = 5, -- any positive integer
+                -- position of quickfix window
+                qf_window_pos = "bottom", -- "bottom", "right", "left" or "top"
+                -- "true" does not open picker for single result, just JUMP
+                skip_picker_for_single_result = false, -- "false" or "true"
+                -- these args are directly passed to "cscope -f <db_file> <args>"
+                db_build_cmd_args = { "-bqkv" },
+                -- statusline indicator, default is cscope executable
+                statusline_indicator = nil,
+            }
+        },
+    },
+    'nvim-telescope/telescope-ui-select.nvim',
 })
 
 -- coc for Frostbite
 vim.g.coc_global_extensions = {'coc-clangd'}
 
--- basic configs
 vim.opt.encoding   = 'utf-8'
 vim.opt.nu         = true
 vim.opt.autoindent = true
