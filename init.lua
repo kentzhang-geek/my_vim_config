9ebb97c8783d78d1aff123d92c91cdf4847d59dd
Kent <tyzhang108@gmail.com>
2024-10-15 16:27:37 -0700

    p4 cmd need fix

--- init.lua
+++ init.lua
@@ -657,8 +657,8 @@ function UtilityMenu()
         elseif sel == 'cd to file' then
             vim.cmd('cd ' .. file_path)
         elseif sel == 'p4 edit' then
-            vim.cmd('cd ' .. file_path)
-            vim.cmd('!p4 edit ' .. filename)
+            vim.cmd('cd \"' .. file_path .. '\"')
+            vim.cmd('!p4 edit \"' .. filename .. '\"')
         elseif sel == 'codelink tools' then
             CodeLinkMenu(filename, linenum)
         elseif sel == 'minimap' then
