b5aca21082e390916c9d523b64d063dab49253c4
Kent <tyzhang108@gmail.com>
2026-01-21 15:45:24 -0800

    use avante instead of github copilot

--- init.lua
+++ init.lua
@@ -105,20 +105,65 @@ require('lazy').setup({
         build = function ()
             vim.cmd(':call fzf#install()')
         end
-    },
-    'junegunn/fzf.vim',
-    'github/copilot.vim',
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        dependencies = {
-            { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-            { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-        },
-        opts = {
-            -- See Configuration section for options
-        },
-        -- See Commands section for default commands if you want to lazy load on them
-    },
+	},
+	'junegunn/fzf.vim',
+	{
+		"yetone/avante.nvim",
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		-- ⚠️ must add this setting! ! !
+		build = vim.fn.has("win32") ~= 0
+		and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+		or "make",
+		event = "VeryLazy",
+		version = false, -- Never set this value to "*"! Never!
+		---@module 'avante'
+		---@type avante.Config
+		opts = {
+			-- add any opts here
+			-- this file can contain specific instructions for your project
+			instructions_file = "avante.md",
+			-- for example
+			provider = "copilot",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"nvim-mini/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"stevearc/dressing.nvim", -- for input provider dressing
+			"folke/snacks.nvim", -- for input provider snacks
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				'MeanderingProgrammer/render-markdown.nvim',
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
     'junegunn/vim-easy-align',
     'frazrepo/vim-rainbow',
     {
@@ -388,7 +433,6 @@ endfunction
 ]])
 
 if vim.fn.has('gui_running') then
-    vim.cmd('set guioptions+=c')
     vim.cmd('set guifont=Consolas:h16')
     vim.keymap.set('n', '<C-ScrollWheelUp>', ':call AdjustFontSize(2)<CR>')
     vim.keymap.set('n', '<C-ScrollWheelDown>', ':call AdjustFontSize(-2)<CR>')
@@ -474,15 +518,16 @@ vim.keymap.set('n', nleader .. 'ea', 'vip<Plug>(EasyAlign)<C-X>')
 -- vim.keymap.set('i', ileader .. 's', '<Plug>(easymotion-sn)')
 
 -- For Copilot
-local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-local Plug_opts = {silent = true, noremap = false}
-vim.g.copilot_no_tab_map = true
-vim.keymap.set(nimode, '<M-\\>', '<Plug>(copilot-suggest)', Plug_opts)
-vim.keymap.set(nimode, '<M-r>', '<Plug>(copilot-suggest)', Plug_opts)
-vim.keymap.set(nimode, '<M-n>', '<Plug>(copilot-next)', Plug_opts)
-vim.keymap.set(nimode, '<M-p>', '<Plug>(copilot-prev)', Plug_opts)
-vim.keymap.set(nimode, '<M-x>', 'copilot#Accept()', opts)
-vim.cmd(':Copilot enable')
+-- local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+-- local Plug_opts = {silent = true, noremap = false}
+-- vim.g.copilot_no_tab_map = true
+-- vim.keymap.set(nimode, '<M-\\>', '<Plug>(copilot-suggest)', Plug_opts)
+-- vim.keymap.set(nimode, '<M-r>', '<Plug>(copilot-suggest)', Plug_opts)
+-- vim.keymap.set(nimode, '<M-n>', '<Plug>(copilot-next)', Plug_opts)
+-- vim.keymap.set(nimode, '<M-p>', '<Plug>(copilot-prev)', Plug_opts)
+-- vim.keymap.set(nimode, '<M-x>', 'copilot#Accept()', opts)
+-- vim.cmd(':Copilot enable')
+require("copilot").setup({})
 
 -- For basic completion
 vim.keymap.set('i', '<Tab>', 'coc#pum#visible() ? coc#pum#confirm() : "<Tab>"', opts)
