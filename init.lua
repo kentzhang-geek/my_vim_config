b15f4fca4ca13097969ee647f9803e1956aaa053
Kent Zhang <tyzhang108@gmail.com>
2026-01-29 19:09:03 -0800

    bookmark update

--- init.lua
+++ init.lua
@@ -345,7 +345,7 @@ require('lazy').setup({
                     ["@t"] = "☑️", -- mark annotation startswith @t ,signs this icon as `Todo`
                     ["@w"] = "⚠️", -- mark annotation startswith @w ,signs this icon as `Warn`
                     ["@f"] = "⛏", -- mark annotation startswith @f ,signs this icon as `Fix`
-                    ["@n"] = "", -- mark annotation startswith @n ,signs this icon as `Note`
+                    ["@n"] = "⭐", -- mark annotation startswith @n ,signs this icon as `Note`
                 },
             }
         end
