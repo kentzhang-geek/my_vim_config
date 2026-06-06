993dcd9dcf487a9f39321e48401a065743e7d508
Kent Zhang <tyzhang108@gmail.com>
2026-06-05 19:33:07 -0700

    fix something

--- init.lua
+++ init.lua
@@ -457,7 +457,7 @@ require('lazy').setup({
 		-- setting the keybinding for LazyGit with 'keys' is recommended in
 		-- order to load the plugin when the command is run for the first time
 		keys = {
-			{ "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+			{ "<leader>lg", "<cmd>cd %:p:h<cr><cmd>LazyGit<cr>", desc = "LazyGit" }
 		}
 	},
 	{
