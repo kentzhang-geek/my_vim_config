d91871c7564ed04317665a8a13399fe002faf47d
Kent <tyzhang108@gmail.com>
2026-06-18 15:38:46 -0700

    add noice for lsp hover doc

--- init.lua
+++ init.lua
@@ -249,6 +249,62 @@ require('lazy').setup({
 		event = "VeryLazy",
 		opts = {
 			-- add any options here
+			cmdline = {
+				enabled = false, -- disabled to avoid conflict with wilder.nvim
+				view = "cmdline_popup", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+				opts = {}, -- global options for the cmdline. See section on views
+				---@type table<string, CmdlineFormat>
+				format = {
+					-- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+					-- view: (default is cmdline view)
+					-- opts: any options passed to the view
+					-- icon_hl_group: optional hl_group for the icon
+					-- title: set to anything or empty string to hide
+					cmdline = { pattern = "^:", icon = "", lang = "vim" },
+					search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+					search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+					filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+					lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
+					help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+					input = { view = "cmdline_input", icon = "󰥻 " }, -- Used by input()
+					-- lua = false, -- to disable a format, set to `false`
+				},
+			},
+			lsp = {
+				hover = {
+					enabled = true,
+					silent = false, -- set to true to not show a message if hover is not available
+					view = nil, -- when nil, use defaults from documentation
+					---@type NoiceViewOptions
+					opts = {}, -- merged with defaults from documentation
+				},
+				signature = {
+					enabled = true,
+					auto_open = {
+						enabled = true,
+						trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+						luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+						throttle = 50, -- Debounce lsp signature help request by 50ms
+					},
+					view = nil, -- when nil, use defaults from documentation
+					---@type NoiceViewOptions
+					opts = {}, -- merged with defaults from documentation
+				},
+				-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+			-- you can enable a preset for easier configuration
+			presets = {
+				bottom_search = true, -- use a classic bottom cmdline for search
+				command_palette = true, -- position the cmdline and popupmenu together
+				long_message_to_split = true, -- long messages will be sent to a split
+				inc_rename = false, -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = true, -- add a border to hover docs and signature help
+			},
 		},
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -783,7 +839,7 @@ require("aerial").setup({
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
 vim.keymap.set("n", "<leader>ol", "<cmd>Telescope aerial<CR>")
 
-vim.keymap.set("n", "<M-d>", vim.lsp.buf.hover, { desc = "doc" })
+vim.keymap.set("n", "<M-d>", function() vim.lsp.buf.hover() end, { desc = "doc" })
 
 --! @brief Set up Copilot with suggestion/panel configurations.
 function SetupCopilot()
